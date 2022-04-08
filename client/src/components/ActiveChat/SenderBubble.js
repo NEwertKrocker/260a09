4f60b929c4bde0bd1662984aa64c24a8b8c112b7
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2022-04-08 11:38:21 -0400

    Add UI for different image display arrangments

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -24,18 +24,49 @@ const useStyles = makeStyles(() => ({
   bubble: {
     background: '#F4F6FA',
     borderRadius: '10px 10px 0 10px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: "flex-end",
+    textAlign: "center",
+  },
+  image: {
+    display: "block",
+    width: "20%",
+    borderRadius: '10px 10px 0px 0px',
+  },
+  imgThumb: {
+    display: "inline-block",
+    width: "100%",
+    borderRadius: '10px 10px 0px 10px',
+    padding: "5px"
+  },
+  imgGrid: {
+    borderRadius: '10px 10px 0 10px',
+    display: 'grid',
+    gridTemplateColumns: "repeat(2, 1fr)",
+    justifyItems: "end",
+    width: "15%"
   },
 }));
 
-const SenderBubble = ({ time, text }) => {
+const SenderBubble = ({ time, text, attachments }) => {
   const classes = useStyles();
+
+  let multiImages = attachments.map((image) => {
+      return <Box className={classes.imgThumb} component="img" src={image} />
+    })
 
   return (
     <Box className={classes.root}>
-      <Typography className={classes.date}>{time}</Typography>
+      {(attachments.length < 2) && <Typography className={classes.date}>{time}</Typography>}
       <Box className={classes.bubble}>
+        {(attachments.length === 1) && <Box className={classes.image} component="img" src={attachments[0]} />}
         <Typography className={classes.text}>{text}</Typography>
       </Box>
+      {(attachments.length > 1) && <Box className={classes.imgGrid}>
+        {multiImages}
+      </Box>}
+      {(attachments.length > 1) && <Typography className={classes.date}>{time}</Typography>}
     </Box>
   );
 };
